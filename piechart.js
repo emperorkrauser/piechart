3cc46580abc5afbea562665beaa23196ac32dd4f
Christian <krauserchristian@gmail.com>
2020-07-17 12:46:24 +0800

    Added legend on the right side of the pie graph

--- piechart.js
+++ piechart.js
@@ -8,11 +8,11 @@ const loadJSON = () => {
 }
 
 const svg = async () => {
-  const width = 600;
+  const width = 850;
   const height = 500;
   const colors = d3.scaleOrdinal(d3.schemeDark2);
   const radius = Math.min(width, height) / 2;
-  const svg = d3.select("body").append("svg").attr("width", width).attr("height", height).style("background", "white");
+  const svg = d3.select("body").append("svg").attr("width", width).attr("height", height).style("background", "#ffffff");
 
   // getting the json data
   const jsonData = await loadJSON();
@@ -29,10 +29,22 @@ const svg = async () => {
 
   // specify the labels
   const content = d3.select("g").selectAll("text").data(data);
-  content.enter().append("text").each(function(d) {
-    const center = arc.centroid(d);
-    d3.select(this ).attr("x", center[0]).attr("y", center[1]).text(d.data.id + 1);
+  content.enter().append("text").attr("transform", function(d) {
+    return "translate(" + arc.centroid(d) + ")";
+  }).attr("dy", "5px").attr("dx", "0px").style("text-anchor", "middle").text(function(d){
+    //can put a label here
+    return d.data.id + 1;
   });
+
+  // adding a legend
+  const legend = svg.append("g").attr("transform", "translate(600, 0)").selectAll(".legends").data(data);
+  const legendContainer = legend.enter().append("g").classed("legends", true).attr("transform", function(d,i){
+    return "translate(0, " + (i+1) * 30 + ")";
+  });
+  legendContainer.append("rect").attr("width", 20).attr("height", 20).attr("fill", (d) => colors(d.data.id));
+  legendContainer.append("text").classed("label", true).text((d) => {
+    return d.data.name;
+  }).attr("fill", (d) => colors(d.data.id)).attr("x", 30).attr("y", 14); //adjust the x and y accordingly
 
 }
 
